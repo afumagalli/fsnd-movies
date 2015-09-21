2cda90e76a4017a9100f633f429587e619890608
Anthony Fumagalli <anthony.fumagalli@udacity.com>
2015-09-21 16:04:32 -0700

    Add extra information to movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,31 +1,59 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-						"A story of a boy and his toys that come to life",
-						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-						"https://www.youtube.com/watch?v=vwyZH85NQC4")
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that come to life",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=vwyZH85NQC4",
+    "John Lasseter",
+    ["Tom Hanks", "Tim Allen", "Don Rickles", "Jim Varney"],
+    1995)
 
-avatar = media.Movie("Avatar",
-					"A marine on an alien planet",
-					"http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien planet",
+    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+    "James Cameron",
+    ["Sam Worthington", "Stephen Lang", "Sigourney Weaver"],
+    2009)
 
-pulp_fiction = media.Movie("Pulp Fiction",
-							"Gangsters in LA meet divine intervention",
-							"https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg",
-							"https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+pulp_fiction = media.Movie(
+    "Pulp Fiction",
+    "Gangsters in LA meet divine intervention",
+    "https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg",
+    "https://www.youtube.com/watch?v=s7EdQ4FqbhY",
+    "Quentin Tarantino",
+    ["John Travolta", "Samuel L. Jackson", "Uma Thurman", "Bruce Willis"],
+    1994)
 
-lego_movie = media.Movie("The Lego Movie",
-						"An unspecial construction worker tries to save the world",
-						"https://upload.wikimedia.org/wikipedia/en/1/10/The_Lego_Movie_poster.jpg",
-						"https://www.youtube.com/watch?v=fZ_JOBCLF-I")
+lego_movie = media.Movie(
+    "The Lego Movie",
+    "An unspecial construction worker tries to save the world",
+    "https://upload.wikimedia.org/wikipedia/en/1/10/The_Lego_Movie_poster.jpg",
+    "https://www.youtube.com/watch?v=fZ_JOBCLF-I",
+    "Phil Lord",
+    ["Chris Pratt", "Will Ferrell", "Elizabeth Banks", "Will Arnett"],
+    2014)
 
-jurassic_world = media.Movie("Jurassic World",
-							"Things go bad at a theme park filled with live dinosaurs",
-							"https://upload.wikimedia.org/wikipedia/en/6/6e/Jurassic_World_poster.jpg",
-							"https://www.youtube.com/watch?v=RFinNxS5KN4")
+jurassic_world = media.Movie(
+    "Jurassic World",
+    "Things go bad at a theme park filled with live dinosaurs",
+    "https://upload.wikimedia.org/wikipedia/en/6/6e/Jurassic_World_poster.jpg",
+    "https://www.youtube.com/watch?v=RFinNxS5KN4",
+    "Colin Trevorrow",
+    ["Chris Pratt", "Bryce Dallas Howard", "Vincent D'Onofrio", "Ty Simpkins"],
+    2015)
 
+inside_out = media.Movie(
+    "Inside Out",
+    "A young girls emotions deal with an impending disaster after moving to San Francisco",
+    "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",
+    "https://www.youtube.com/watch?v=seMwpP0yeu4",
+    "Pete Docter",
+    ["Amy Poehler", "Phyllis Smith", "Bill Hader", "Lewis Black", "Mindy Kaling"],
+    2015)
 
-movies = [toy_story, avatar, pulp_fiction, lego_movie, jurassic_world]
+movies = [toy_story, avatar, pulp_fiction, lego_movie, jurassic_world, inside_out]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -5,11 +5,17 @@ class Movie():
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                trailer_youtube, director, cast, year):
+        # initializes an instance of class Movie
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.director = director
+        self.cast = cast
+        self.year = year
 
     def show_trailer(self):
+        # opens a movie's trailer (for testing, not needed to make page)
         webbrowser.open(self.trailer_youtube_url)
